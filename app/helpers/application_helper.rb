e0e0e68b84b7852ee099f37a0b77a8ee109f7956
Ryder Moody <rydthemoodster@gmail.com>
2011-06-30 15:46:34 -0700

    bug/users view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,20 +8,12 @@ module ApplicationHelper
   
   def schools
     if signed_in?
-      all_schools_for_college=[]
-      College.find(current_user.college_id).schools.each do |school|
-        all_schools_for_college<<school.name
-      end
-        all_schools_for_college
+        all_schools_for_college=College.find(current_user.college_id).schools
     end
   end
   
   def dorms
     if signed_in?
-      #all_dorms_for_college=[]
-      #College.find(current_user.college_id).dorms.each do |dorm|
-       # all_dorms_for_college<<dorm.name
-      #end
       all_dorms_for_college=College.find(current_user.college_id).dorms
     end
   end
