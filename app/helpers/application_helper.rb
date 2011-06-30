0413ee93dd1b5ba373bf58f1c1be896eaf91d56e
Ryder Moody <rydthemoodster@gmail.com>
2011-06-29 21:38:35 -0700

    helpers for sidebar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,10 @@
 module ApplicationHelper
   
+  def college
+    College.find(current_user.college).name
+  end
+  
+  def schools
+    College.find(current_user.college).schools.split(', ')
+  end
 end
--- app/helpers/colleges_helper.rb
+++ app/helpers/colleges_helper.rb
@@ -1,9 +1,3 @@
 module CollegesHelper
-  def schools(college_id, school_num)
-    if (num>=0)
-      College.find(college_id).schools.split(', ')[school_num]
-    else
-      nil
-    end
-  end
+ 
 end
